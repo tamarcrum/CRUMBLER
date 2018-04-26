b0df5bf5bfc9149544caa178d929049cb3a353a0
Tamar Crum <teagy5@mail.missouri.edu>
2018-04-25 19:48:02 -0500

    Update sw_filt.py
    -- Added a sys.exit for genotypes that are given in the wrong format (anything but AB or 12).
    -- When genotypes are input in AB format: 
    * "A" is specified as the reference allele for all SNPs {plink --reference-allele}
    * A reference allele file is created for input into PLINK {plink --reference-allele ref_allele_[$assay].txt}
    --PLINK -reference-allele fails if duplicate SNP ids are present (only for AB genotypes):
    * New map files are created setting the second occurrence of a duplicate SNP ID to "-1" bp pos (this excludes it from analysis)
    * New map files output to *_filtered/directory

--- src/sw_filt.py
+++ src/sw_filt.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-#Added a --keep-allele-order flag to all plink commands
-
 #Packages
 import sys
 import os
 import re
 from collections import defaultdict
+import pandas as pd
+import csv
 
 def mylistdir(directory):
     """A specialized version of os.listdir() that ignores files that
@@ -29,8 +29,87 @@ with open(mapfiles) as f:
 	mapfiledict = {}
 	for line in f:
 		k,v = line.split(':')
-		v = v.rstrip('\n')
+		v = v.strip()
 		mapfiledict[k] = v
+
+def remove_duplicates(snp_names):
+	output = []
+	seen = set()
+	for snp_id in snp_names:
+		if snp_id not in seen:
+			output.append(snp_id)
+			seen.add(snp_id)
+	return output
+
+def plink_AB_command():
+	for k,v in mapfiledict.items():
+		if k == assay:
+			PLINK_map = v
+
+#Edit map files for duplicate snp IDs which will not work with reference allele specification			
+			v_filename_only = v.split('/')[-1]
+			map_file_table = pd.read_table(PLINK_map, sep='\t', header=None, index_col=False)
+			map_file_table.columns = ['chr', 'snp_id', 'g_dis', 'bpos']
+			map_file_table['dup'] = map_file_table['snp_id'].duplicated().astype(int).astype(str)
+			map_file_table.loc[map_file_table.dup == '1', 'bpos'] = '-1'
+			map_file_table2 = map_file_table.drop('dup', 1)
+			map_file_table2.sort_index(inplace=True)
+			name_of_output_map = path_of_outputs + v_filename_only
+			map_file_table2.to_csv(name_of_output_map, sep='\t', header=False, index=False)
+
+#Create reference allele specification file			
+			ref_allele_filename = path_of_outputs + 'ref_allele_' + assay + '.txt'
+			ref_allele = open(ref_allele_filename, 'w')
+			snp_names = [n[1] for n in csv.reader(open(PLINK_map, 'r'), delimiter = '\t')]
+			snp_names_2 = remove_duplicates(snp_names)
+ 			for s in snp_names_2:
+				ref_allele.write(s + " A" + "\n")
+			ref_allele.close()
+
+#Plink command to filter markers and set reference allele			
+			plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + name_of_output_map + " --extract " + marker_list + " --reference-allele " + path_of_outputs + "ref_allele_" + assay + ".txt  --recode 12 --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
+			os.system(plink_filter_markers_command)
+
+			 
+			
+		elif len(mapfiledict) <= 1:
+			k,v = mapfiledict.items()[0]
+			PLINK_map = v
+#Edit map file for duplicate snp IDs which will not work with reference allele specification			
+			v_filename_only = v.split('/')[-1]
+			map_file_table = pd.read_table(PLINK_map, sep='\t', header=None, index_col=False)
+			map_file_table.columns = ['chr', 'snp_id', 'g_dis', 'bpos']
+			map_file_table['dup'] = map_file_table['snp_id'].duplicated().astype(int).astype(str)
+			map_file_table.loc[map_file_table.dup == '1', 'bpos'] = '-1'
+			map_file_table2 = map_file_table.drop('dup', 1)
+			map_file_table2.sort_index(inplace=True)
+			name_of_output_map = path_of_outputs + v_filename_only
+			map_file_table2.to_csv(name_of_output_map, sep='\t', header=False, index=False)
+
+#Create reference allele specification file			
+			ref_allele_filename = path_of_outputs + 'ref_allele_' + assay + '.txt'
+			ref_allele = open(ref_allele_filename, 'w')
+			snp_names = [n[1] for n in csv.reader(open(PLINK_map, 'r'), delimiter = '\t')]
+			snp_names_2 = remove_duplicates(snp_names)
+ 			for s in snp_names_2:
+				ref_allele.write(s + " A" + "\n")
+			ref_allele.close()
+
+#Plink command to filter markers and set reference allele			
+			plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + name_of_output_map + " --extract " + marker_list + " --reference-allele " + path_of_outputs + "ref_allele_" + assay + ".txt  --recode 12 --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
+			os.system(plink_filter_markers_command)
+	
+def plink_12_command():
+	for k,v in mapfiledict.items():
+		if k == assay:
+			PLINK_map = v
+			plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
+			os.system(plink_filter_markers_command)
+		elif len(mapfiledict) <= 1:
+			k,v = mapfiledict.items()[0]
+			PLINK_map = v
+			plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
+			os.system(plink_filter_markers_command)
 
 
 #Making Output Directory
@@ -39,36 +118,34 @@ if not os.path.exists(path_of_outputs):
 
 file_names_list = mylistdir(path_of_inputs)
 ped_file_list = []
-for x in file_names_list:
-	if x.split('.')[-1] == "ped":   
-		ped_file_list.append(x)
+for b in file_names_list:
+	if b.split('.')[-1] == "ped":   
+		ped_file_list.append(b)
 
+nonempty_genotype_files = []
+for c in ped_file_list:
+	path_file_name = path_of_inputs + c
+	if os.path.getsize(path_file_name) > 0:
+		nonempty_genotype_files.append(c)
+
+
+	
 
 counter = 0
-for x in ped_file_list:	
-	path_file_name = path_of_inputs + x
-	if os.path.getsize(path_file_name) > 0:
-		counter +=1
-		assay = x.split('.')[-2]
-		for k,v in mapfiledict.items():
-			if k == assay:
-				PLINK_map = v
-				plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --keep-allele-order --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
-				os.system(plink_filter_markers_command)
-				# if filter_plink_call_rate == 'Y':
-# 					plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --recode --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
-# 					os.system(plink_filter_markers_command)
-# 				else:
-# 					plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
-# 					os.system(plink_filter_markers_command)
-			elif len(mapfiledict) <= 1:
-				k,v = mapfiledict.items()[0]
-				PLINK_map = v
-				plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --keep-allele-order --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
-				os.system(plink_filter_markers_command)
-				# if filter_plink_call_rate == 'Y':
-# 					plink_filter_markers_command = 
-# 					os.system(plink_filter_markers_command)
-# 				else:
-# 					plink_filter_markers_command = "plink --ped " + path_of_inputs + x + " --map " + PLINK_map + " --extract " + marker_list + " --recode --allow-no-sex --" + plink_species_ID + " --out " + path_of_outputs + x + "_filt_" + str(counter)
-# 					os.system(plink_filter_markers_command)
+for x in nonempty_genotype_files:	
+	counter +=1
+	assay = x.split('.')[-2]
+	genotype_file_name = path_of_inputs + x
+	with open(genotype_file_name) as f:
+		first_line = f.readline().strip()
+		if 'BB' in first_line:
+			plink_AB_command()
+		elif 'C' in first_line: 
+			sys.exit('Genotypes are not in AB or 12 format.')
+		elif 'T' in first_line: 
+			sys.exit('Genotypes are not in AB or 12 format.')
+		else:
+			plink_12_command()
+
+		
+				
